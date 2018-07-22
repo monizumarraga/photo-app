11da54c88d594cb601cd85a793cc659b08c1ac20
monizumarraga <monizumarraga.hotmail.com>
2018-07-22 18:47:51 +0000

    add devise and bootstrap

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '3c3d967ed73dd6a2f986632f33a5fdb96aaa48b4598b908d8577a03556cf72571fab0cf159689f15fcbaf5a785102a38e75cfd9f9afae155ab478674ed0c5796'
+   config.secret_key = '3c3d967ed73dd6a2f986632f33a5fdb96aaa48b4598b908d8577a03556cf72571fab0cf159689f15fcbaf5a785102a38e75cfd9f9afae155ab478674ed0c5796'
   
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
